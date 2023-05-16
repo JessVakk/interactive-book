b981672212f9ccc8303c1a9a5cdbe932baad65f2
Jessica Vakkuri <jessica.vakkuri@utb.ecutbildning.se>
2023-05-16 15:53:20 +0200

    testing images

--- src/views/Page1.jsx
+++ src/views/Page1.jsx
@@ -10,17 +10,19 @@ const Page1 = () => {
         return <Navigate to="/sida2" />;
     }
   return (
-    <div className='square'>
-        <div className='book-text-wrapper'>
-          <p className='book-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora pariatur iure error fuga quos id soluta. Veniam quis placeat nostrum consequuntur aperiam dignissimos cum minima quibusdam, molestias sit, enim quia?</p>
-          <br />
-          <p className='book-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, quo facilis molestiae autem pariatur, quod eum dignissimos tempora cupiditate eaque ex iusto! Id explicabo magnam aspernatur excepturi obcaecati, eum molestiae.</p>
-        </div>
-        <div className='btn-container'>
-          <button className='btn' onClick={() => {setNextPage(true);}}>
-          {" "} Gå vidare </button>
-        </div>
+    <div>
+      <div className='square'>
+          <div className='book-text-wrapper'>
+            <p className='book-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora pariatur iure error fuga quos id soluta. Veniam quis placeat nostrum consequuntur aperiam dignissimos cum minima quibusdam, molestias sit, enim quia?</p>
+            <br />
+            <p className='book-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, quo facilis molestiae autem pariatur, quod eum dignissimos tempora cupiditate eaque ex iusto! Id explicabo magnam aspernatur excepturi obcaecati, eum molestiae.</p>
+          </div>
+          <div className='btn-container'>
+            <button className='btn' onClick={() => {setNextPage(true);}}>
+            {" "} Gå vidare </button>
+          </div>
       </div>
+    </div>
   )
 }
 
--- src/views/path2/Page3A.jsx
+++ src/views/path2/Page3A.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import img1 from '../../assets/vännerna.png';
 
 const Page3A = () => {
   return (
-    <div>Page3A</div>
+    <div>
+      <section>
+        <div className='splitScren'>
+          <div className='column'>
+            <div className='leftSide'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda minus quas, fugiat nisi distinctio cumque fuga inventore illo architecto accusamus iure, ipsa, quo maiores consequatur odio error consectetur est aut.</p>
+            </div>
+          </div>
+          <div className='column'>
+            <div className='rightSide'>
+                <img className='pageImg' src={img1} />
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
   )
 }
 
